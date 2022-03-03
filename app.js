f76b4be6a5922166baf0d8933b311f3b82b5211b
SabrinaMannan <sabrinameem21@gmail.com>
2022-03-04 05:08:29 +0600

    temperature changes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const API_KEY = `481ae12c3bfe9ed30a57a352f787efc4`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data));
 }
 
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
 const displayTemperature = temperature => {
+
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
     console.log(temperature);
 }
